68b6638b90bcc882a46c07b3cdfcbe9b16d0ccc3
Taani Maama <taani1maama@live.com>
2020-11-20 17:26:30 -0800

    Add the script need for my page to filter to the sections the user wants to view.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,26 @@
-$(nav).on('click', '.btn', handleSections)
+$('#home').ready().removeClass('hide')
+
+$('nav').on('click', '.btn', handleSections)
 
 function handleSections(event) {
-    event.preventDefault();
+    // event.preventDefault();
 
-    if (this.data('section') == 'about-me') {
-        console.log('hello')
+    if ($(this).data('section') == 'home') {
+        $('#about-me').addClass('hide')
+        $('#projects').addClass('hide')
+        $('#home').removeClass('hide')
     }
+
+    if ($(this).data('section') == 'about-me') {
+        $('#home').addClass('hide')
+        $('#projects').addClass('hide')
+        $('#about-me').removeClass('hide')
+    }
+
+    if ($(this).data('section') == 'projects') {
+        $('#home').addClass('hide')
+        $('#about-me').addClass('hide')
+        $('#projects').removeClass('hide')
+    };
+
 }
