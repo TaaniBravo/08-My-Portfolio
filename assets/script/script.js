d3d6512b06b3d72f2a8763eabed187ac194d28c6
Taani Maama <taani1maama@live.com>
2020-11-22 20:44:31 -0800

    Again some major edits especially front-end this time using the media query so that I could make the navigation look clean on mobile and transition back just fine just based on the users screen width.

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,13 +4,37 @@ const $projectsEl = $('#projects')
 
 $('nav').on('click', '.btn', handleSections)
 
+// Upon intial load of the page we want this open if statement to make the page be responsive to the size of the screen.
+if (window.innerWidth <= 768) {
+    if ($('nav').hasClass('flex-column')) {
+        $('nav').removeClass('flex-column')
+        $('nav').addClass('flex-row')
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 768) {
+        if ($('nav').hasClass('flex-column')) {
+            $('nav').removeClass('flex-column')
+            $('nav').addClass('flex-row')
+        }
+    }
+
+    if (window.innerWidth > 768) {
+        if ($('nav').hasClass('flex-row')) {
+            $('nav').removeClass('flex-row')
+            $('nav').addClass('flex-column')
+        }
+    }
+});
+
 function handleSections(event) {
     // event.preventDefault();
 
     if ($(this).data('section') == 'home') {
         $aboutMeEl.addClass('trans')
         $projectsEl.addClass('trans')
-        setTimeout(() =>  {
+        setTimeout(() => {
             $aboutMeEl.addClass('hide')
             $projectsEl.addClass('hide')
             $homeEl.removeClass('hide')
@@ -21,7 +45,7 @@ function handleSections(event) {
     if ($(this).data('section') == 'about-me') {
         $homeEl.addClass('trans')
         $projectsEl.addClass('trans')
-        setTimeout(() =>  {
+        setTimeout(() => {
             $homeEl.addClass('hide')
             $projectsEl.addClass('hide')
             $aboutMeEl.removeClass('hide')
@@ -32,7 +56,7 @@ function handleSections(event) {
     if ($(this).data('section') == 'projects') {
         $homeEl.addClass('trans')
         $aboutMeEl.addClass('trans')
-        setTimeout(() =>  {
+        setTimeout(() => {
             $homeEl.addClass('hide')
             $aboutMeEl.addClass('hide')
             $projectsEl.removeClass('hide')
