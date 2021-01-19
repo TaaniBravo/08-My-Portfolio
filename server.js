88c4217a50070d9b9d751dc949356b5e7edf6c20
Taani Maama <taani1maama@live.com>
2021-01-18 18:55:10 -0800

    Updated the code to work with handlebars. Going to update the current projects on my page with projects that I am finishing up soon.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
@@ -7,9 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
-});
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+require('./routes/htmlRoutes')(app)
 
 app.listen(PORT, err => {
     if (err) console.error(err);
