2d0a6dbab4cfe92e71d745ffcdde15575788656f
Taani Maama <taani1maama@live.com>
2021-02-09 17:33:11 -0800

    Added compression for better performance.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const compression = require("compression");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
+app.use(compression());
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
